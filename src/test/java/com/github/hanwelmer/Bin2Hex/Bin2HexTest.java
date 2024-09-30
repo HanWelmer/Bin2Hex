f37c4f5bef5d01a7526e05bba0e57f9c930e1f48
HanOfBea <han.welmer@tno.nl>
2024-09-30 23:23:49 +0200

    Add unit test.

--- src/test/java/com/github/hanwelmer/Bin2Hex/Bin2HexTest.java
+++ src/test/java/com/github/hanwelmer/Bin2Hex/Bin2HexTest.java
@@ -1,38 +1,57 @@
 package com.github.hanwelmer.Bin2Hex;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.assertTrue;
 
-/**
- * Unit tests.
- */
-public class Bin2HexTest
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public Bin2HexTest( String testName )
-    {
-        super( testName );
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.Test;
+
+public class Bin2HexTest {
+
+  /**
+   * Test usage output if no parameters are passed.
+   */
+  @Test
+  public void testUsage() {
+    // Create a stream to hold the output
+    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    PrintStream ps = new PrintStream(baos);
+    // IMPORTANT: Save the old System.out!
+    PrintStream old = System.out;
+    // Tell Java to use your special stream
+    System.setOut(ps);
+
+    // Set up SecurityManager to catch the exit(1).
+    System.setSecurityManager(new SecurityManager() {
+      @Override
+      public void checkExit(int status) {
+        super.checkExit(status);
+        if (status != 0) {
+          throw new SecurityException("Intercepted exit with status: " + status);
+        }
+      }
+
+      @Override
+      public void checkPermission(java.security.Permission perm) {
+        // Allow other permissions
+      }
+    });
+
+    try {
+      // Do the test, with System.out redirected to our ps PrintStream.
+      String[] args = { "", "" };
+      Bin2Hex.main(args);
+    } catch (SecurityException e) {
+      System.out.println("Caught exit: " + e.getMessage());
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( Bin2HexTest.class );
-    }
+    // Put things back
+    System.out.flush();
+    System.setOut(old);
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testUsage()
-    {
-        assertTrue( true );
-    }
+    // Test what happened
+    // assertTrue(baos.size() == 0);
+    assertTrue(baos.toString().startsWith("Usage: java -jar Bin2Hex.jar filename.ext"));
+  }
 }
